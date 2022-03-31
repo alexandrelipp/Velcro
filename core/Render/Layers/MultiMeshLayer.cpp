5e5107e795411ee1dcb4f41b25141f6150b62c4f
Alexandre Lipp <alexandre.lipp@gmail.com>
2022-03-31 06:22:28 -0400

    hard

--- core/Render/Layers/MultiMeshLayer.cpp
+++ core/Render/Layers/MultiMeshLayer.cpp
@@ -20,10 +20,10 @@ MultiMeshLayer::MultiMeshLayer(VkRenderPass renderPass) {
         buffer.init(_vrd->device, _vrd->physicalDevice, sizeof(glm::mat4));
 
     _scene = std::make_shared<Scene>("NanoWorld");
-    //FactoryModel::importFromFile("../../../core/Assets/Models/Nano/nanosuit.obj", _scene);
+    FactoryModel::importFromFile("../../../core/Assets/Models/Nano/nanosuit.obj", _scene);
     //FactoryModel::importFromFile("../../../core/Assets/Models/utahTeapot.fbx", _scene);
     //FactoryModel::importFromFile("../../../core/Assets/Models/cube.fbx", _scene);
-    FactoryModel::importFromFile("../../../core/Assets/Models/Bell Huey.fbx", _scene);
+    //FactoryModel::importFromFile("../../../core/Assets/Models/Bell Huey.fbx", _scene);
     //FactoryModel::importFromFile("../../../core/Assets/Models/duck/scene.gltf", _scene);
 
     static_assert(sizeof(Vertex) == sizeof(Vertex::position) + sizeof(Vertex::normal) + sizeof(Vertex::uv));
