0f959faef53410dc0fce3e812ea765254b13a5ee
Alexandre Lipp <alexandre.lipp@gmail.com>
2022-03-31 20:20:46 -0400

    added vali

---
+++ core/Assets/Shaders/glslangValidator.bat
@@ -0,0 +1,2 @@
+C:\VulkanSDK\1.3.204.1\Bin\glslangValidator.exe -V100 --target-env spirv1.3 %1.vert -o bin\%1V.spv
+C:\VulkanSDK\1.3.204.1\Bin\glslangValidator.exe -V100 --target-env spirv1.3 %1.frag -o bin\%1F.spv
