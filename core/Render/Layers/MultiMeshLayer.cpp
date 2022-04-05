d9852d93b83b65013ae66f8fdd862a1510a9f67f
Alexandre Lipp <alexandre.lipp@gmail.com>
2022-04-05 07:45:16 -0400

    added DS model

--- core/Render/Layers/MultiMeshLayer.cpp
+++ core/Render/Layers/MultiMeshLayer.cpp
@@ -20,9 +20,9 @@ MultiMeshLayer::MultiMeshLayer(VkRenderPass renderPass) {
         buffer.init(_vrd->device, _vrd->physicalDevice, sizeof(glm::mat4));
 
     _scene = std::make_shared<Scene>("NanoWorld");
-    FactoryModel::importFromFile("../../../core/Assets/Models/Nano/nanosuit.obj", _scene);
+    //FactoryModel::importFromFile("../../../core/Assets/Models/Nano/nanosuit.obj", _scene);
     //FactoryModel::importFromFile("../../../core/Assets/Models/utahTeapot.fbx", _scene);
-    //FactoryModel::importFromFile("../../../core/Assets/Models/cube.fbx", _scene);
+    FactoryModel::importFromFile("../../../core/Assets/Models/engine.fbx", _scene);
     //FactoryModel::importFromFile("../../../core/Assets/Models/Bell Huey.fbx", _scene);
     //FactoryModel::importFromFile("../../../core/Assets/Models/duck/scene.gltf", _scene);
 
