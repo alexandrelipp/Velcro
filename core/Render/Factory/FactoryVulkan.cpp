4d36822b8ea7d6d2371c0de053449b518a27b1cb
Alexandre Lipp <alexandre.lipp@gmail.com>
2022-04-09 14:58:40 -0400

    beofre first try

--- core/Render/Factory/FactoryVulkan.cpp
+++ core/Render/Factory/FactoryVulkan.cpp
@@ -579,6 +579,13 @@ namespace Factory {
         VK_CHECK(vkCreatePipelineLayout(renderDevice->device, &pipelineLayoutInfo, nullptr, &pipelineLayout));
 
         // create descriptor pool
+        // Inadequate descriptor pools are a good example of a problem that the validation layers will not catch:
+        // As of Vulkan 1.1, vkAllocateDescriptorSets may fail with the error code VK_ERROR_POOL_OUT_OF_MEMORY
+        // if the pool is not sufficiently large, but the driver may also try to solve the problem internally.
+        // This means that sometimes (depending on hardware, pool size and allocation size) the driver will let
+        // us get away with an allocation that exceeds the limits of our descriptor pool.
+        // Other times, vkAllocateDescriptorSets will fail and return VK_ERROR_POOL_OUT_OF_MEMORY.
+        // This can be particularly frustrating if the allocation succeeds on some machines, but fails on others.
         VkDescriptorPool descriptorPool = Factory::createDescriptorPool(renderDevice->device, MAX_FRAMES_IN_FLIGHT,
                                                                         uniformBufferCount, storageBufferCount, samplerImageCount);
 
