2684c39a07173feac8d7c4cb0dd7485f9d3d8ad7
Alexandre Lipp <alexandre.lipp@gmail.com>
2022-04-10 20:01:05 -0400

    winding of base stuff:

--- core/Render/Factory/FactoryModel.cpp
+++ core/Render/Factory/FactoryModel.cpp
@@ -41,14 +41,14 @@ bool FactoryModel::createDuckModel(std::vector<TexVertex>& vertices, std::vector
 
 bool FactoryModel::createTexturedSquare(std::vector<TexVertex>& vertices, std::vector<uint32_t>& indices) {
     vertices = {
-            {glm::vec3(-0.5f, -0.5f, 0.f), glm::vec2(0.f, 0.f)},// top left
             {glm::vec3(0.5f, -0.5f, 0.f),  glm::vec2(1.f, 0.f)},// top right
+            {glm::vec3(-0.5f, -0.5f, 0.f), glm::vec2(0.f, 0.f)},// top left
             {glm::vec3(0.5f, 0.5f, 0.f),   glm::vec2(1.f, 1.f)},// bottom right
             {glm::vec3(-0.5f, 0.5f, 0.f),  glm::vec2(0.f, 1.f)},// bottom left
     };
 
     indices = {
-            0, 1, 2, 2, 3, 0
+            0, 1, 2, 2, 1, 3
     };
     return true;
 }
@@ -56,12 +56,12 @@ bool FactoryModel::createTexturedSquare(std::vector<TexVertex>& vertices, std::v
 
 bool FactoryModel::createTexturedSquare2(std::vector<TexVertex2>& vertices) {
     vertices = {
-            {glm::vec2(-0.5f, -0.5f), glm::vec2(0.f, 0.f)},// top left
             {glm::vec2( 0.5f, -0.5f), glm::vec2(1.f, 0.f)},// top right
-            {glm::vec2( 0.5f,  0.5f), glm::vec2(1.f, 1.f)},// bottom right
-            {glm::vec2( 0.5f,  0.5f), glm::vec2(1.f, 1.f)},// bottom right
-            {glm::vec2(-0.5f,  0.5f), glm::vec2(0.f, 1.f)},// bottom left
             {glm::vec2(-0.5f, -0.5f), glm::vec2(0.f, 0.f)},// top left
+            {glm::vec2( 0.5f,  0.5f), glm::vec2(1.f, 1.f)},// bottom right
+            {glm::vec2( 0.5f,  0.5f), glm::vec2(1.f, 1.f)},// bottom right
+            {glm::vec2(-0.5f, -0.5f), glm::vec2(0.f, 0.f)},// top left
+            {glm::vec2(-0.5f,  0.5f), glm::vec2(0.f, 1.f)},// bottom left
     };
     return true;
 }
