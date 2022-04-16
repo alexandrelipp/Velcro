213598cb79be6e24d2f9749be14179ed0693484f
Alexandre Lipp <alexandre.lipp@gmail.com>
2022-04-15 20:13:10 -0400

    low poly truck

--- core/Render/Layers/MultiMeshLayer.cpp
+++ core/Render/Layers/MultiMeshLayer.cpp
@@ -25,6 +25,14 @@ MultiMeshLayer::MultiMeshLayer(VkRenderPass renderPass) {
     //FactoryModel::importFromFile("../../../core/Assets/Models/Nano/nanosuit.obj", _scene);
     //FactoryModel::importFromFile("../../../core/Assets/Models/utahTeapot.fbx", _scene);
     //FactoryModel::importFromFile("../../../core/Assets/Models/engine.fbx", _scene);
+    // https://sketchfab.com/3d-models/low-poly-truck-car-drifter-f3750246b6564607afbefc61cb1683b1
+    FactoryModel::importFromFile("../../../core/Assets/Models/Drifter/source/Jeep_done.fbx", _scene);
+    // nice little model but the default scale is stupid
+    auto& rootTransform = _scene->getTransform(1);
+    rootTransform.scale *= 0.01f;
+    rootTransform.needUpdateModelMatrix = true;
+    _scene->setDirtyTransform(1);
+
     FactoryModel::importFromFile("../../../core/Assets/Models/Bell Huey.fbx", _scene);
     //FactoryModel::importFromFile("../../../core/Assets/Models/duck/scene.gltf", _scene);
 
