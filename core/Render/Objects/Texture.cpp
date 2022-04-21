39dd0541ea6426c95c3f76740ec58c0994a292e6
Alexandre Lipp <alexandre.lipp@gmail.com>
2022-04-20 20:09:02 -0400

    touches

--- core/Render/Objects/Texture.cpp
+++ core/Render/Objects/Texture.cpp
@@ -8,7 +8,7 @@
 #include "../../Utils/UtilsFile.h"
 #include "../../Utils/UtilsVulkan.h"
 
-// TODO : extract in file!
+// TODO : extract in file if used elsewhere
 #define STB_IMAGE_IMPLEMENTATION
 #include <stbi_image.h>
 
@@ -84,6 +84,7 @@ void Texture::init(const Texture::TextureDesc& desc, VulkanRenderDevice& renderD
     if (!createSampler)
         return;
 
+    // create sampler
     VkSamplerCreateInfo samplerCreateInfo = {
             .sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO,
             .flags = 0u,
@@ -102,9 +103,7 @@ void Texture::init(const Texture::TextureDesc& desc, VulkanRenderDevice& renderD
             .unnormalizedCoordinates = VK_FALSE,
 
     };
-
     VK_CHECK(vkCreateSampler(renderDevice.device, &samplerCreateInfo, nullptr, &_sampler));
-
 }
 
 
@@ -119,114 +118,19 @@ void Texture::init(const std::string& filePath, VulkanRenderDevice& renderDevice
     VK_ASSERT(pixels != nullptr, "failed to load texture image!");
 
     // range construct vector
-    std::vector<unsigned char> test(pixels, pixels + imageSize);
-
-    // range construct vector
     std::vector<char> data(pixels, pixels + imageSize);
 
     // free pixels after copy
     stbi_image_free(pixels);
 
+    // create texture description and init texture
     TextureDesc desc = {
             .width = (uint32_t)texWidth,
             .height = (uint32_t)texHeight,
             .imageFormat = VK_FORMAT_R8G8B8A8_SRGB,
             .data = data
     };
-
     init(desc, renderDevice, createSampler);
-    return;
-
-
-
-
-    // create staging buffer for transfer
-    auto stagingBuffer = Factory::createBuffer(renderDevice.device, renderDevice.physicalDevice, imageSize,
-                                               VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
-                                               VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT );
-
-    // copy pixels to staging buffer
-    void* dst = nullptr;
-    VK_CHECK(vkMapMemory(renderDevice.device, stagingBuffer.second, 0, imageSize, 0, &dst));
-    memcpy(dst, pixels, imageSize);
-    vkUnmapMemory(renderDevice.device, stagingBuffer.second);
-
-
-    // format of the image (maybe a function to pick the best one ?)
-    VkFormat imageFormat = VK_FORMAT_R8G8B8A8_SRGB;
-
-    std::tie(_image, _imageMemory) = Factory::createImage(&renderDevice, VK_SAMPLE_COUNT_1_BIT, texWidth, texHeight, imageFormat,
-                                      VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
-                                      VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
-
-    // transition image layout UNDEFINED -> DST_OPTIMAL
-    utils::transitionImageLayout(renderDevice.device, renderDevice.graphicsQueue, renderDevice.commandPool, _image, imageFormat,
-                                 VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
-
-    // copy staging buffer -> image. Note : this should probably be extracted in a utils generic function
-    utils::executeOnQueueSync(renderDevice.graphicsQueue, renderDevice.device, renderDevice.commandPool,
-                              [&, this](VkCommandBuffer commandBuffer){
-        VkBufferImageCopy imageRegion = {
-                .bufferOffset = 0,
-                .bufferRowLength = 0,     // would matter if data was not tightly pacted
-                .bufferImageHeight = 0,   // would matter if data was not tightly pacted
-                .imageSubresource = {
-                        .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
-                        .mipLevel = 0,
-                        .baseArrayLayer = 0,
-                        .layerCount = 1
-                },
-                .imageOffset = {
-                        .x = 0,
-                        .y = 0,
-                        .z = 0,
-                },
-                .imageExtent = {
-                        .width = (uint32_t)texHeight,
-                        .height = (uint32_t)texHeight,
-                        .depth = 1,
-                }
-        };
-        vkCmdCopyBufferToImage(commandBuffer, stagingBuffer.first, _image,
-                               VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &imageRegion);
-    });
-
-
-    // transition from DST_OPTIMAL -> SHADER_READ_ONLY_OPTIMAL
-    utils::transitionImageLayout(renderDevice.device, renderDevice.graphicsQueue, renderDevice.commandPool, _image, imageFormat,
-                                 VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
-
-    // delete the staging buffer after the transfer
-    vkFreeMemory(renderDevice.device, stagingBuffer.second, nullptr);
-    vkDestroyBuffer(renderDevice.device, stagingBuffer.first, nullptr);
-
-    _imageView = Factory::createImageView(renderDevice.device, _image, imageFormat, VK_IMAGE_ASPECT_COLOR_BIT);
-
-    // done initializing if not creating a sampler
-    if (!createSampler)
-        return;
-
-    VkSamplerCreateInfo samplerCreateInfo = {
-            .sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO,
-            .flags = 0u,
-            .magFilter = VK_FILTER_LINEAR,
-            .minFilter = VK_FILTER_LINEAR,
-            .mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR,    // interpolation mode between MIP
-            .addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT,
-            .addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT,
-            .addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT,
-            .mipLodBias = 0.f,                              // Lod bias for mip level
-            .anisotropyEnable = VK_TRUE,                    // enable Ansiotropy, furthest things looks better
-            .maxAnisotropy = 16.f,                          // anisotropy sample level
-            .minLod = 0.f,                                  // min level of detail to pick mip level
-            .maxLod = 0.f,                                  // max level of dtail to pick mip level
-            .borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK, // only applied when repeat mode is clamp to border
-            .unnormalizedCoordinates = VK_FALSE,
-
-    };
-
-    VK_CHECK(vkCreateSampler(renderDevice.device, &samplerCreateInfo, nullptr, &_sampler));
-
 }
 
 void Texture::destroy(VkDevice device) {
