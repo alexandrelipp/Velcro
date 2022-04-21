6b976279acd04a4737630817cf93027adf52c2ec
Alexandre Lipp <alexandre.lipp@gmail.com>
2022-04-21 06:57:27 -0400

    comment

--- core/Render/Factory/FactoryVulkan.cpp
+++ core/Render/Factory/FactoryVulkan.cpp
@@ -339,7 +339,7 @@ namespace Factory {
             // finalColor.rgb = newAlpha * newColor + (1 - newAlpha) * oldColor;
             // finalColor.a = newAlpha.a;
             colorBlendAttachment.blendEnable = VK_TRUE;
-            // Note : Setting srcColorBlendFactor to SRC_COLOR amplifies the colors, but messup blending (less white).
+            // Note : Setting srcColorBlendFactor to SRC_COLOR amplifies the colors (less white), but messup blending .
             // TODO : add as prop option when not caring about blending anyway?
             colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA; // FIXME : why with SRC_COLOR looks way better
             colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
